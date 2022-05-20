4a677fdbf9b2a37f95f8acac7812ac41a0df0dd0
pablobriceno <pabl.briceno@duocuc.cl>
2022-05-19 21:08:54 -0400

    Primera versión de formulario para agregar medicamentos

--- src/views/VistaMedicamentos.vue
+++ src/views/VistaMedicamentos.vue
@@ -1,37 +1,97 @@
 <template>
   <div class="medicamentos">
-    <div class="filters">
-      <div class="filters__item">
-        <input type="text" placeholder="CODIGO" />
+    <template v-if="!modalActive">
+      <div class="filters">
+        <div class="filters__item">
+          <input type="text" placeholder="CODIGO" />
+        </div>
+        <div class="filters__item">
+          <input type="text" placeholder="NOMBRE" />
+        </div>
+        <div class="filters__item">
+          <input type="text" placeholder="CANTIDAD" />
+        </div>
+        <div class="filters__item">
+          <input type="text" placeholder="FABRICANTE" />
+        </div>
+        <div class="filters__item">
+          <input type="text" placeholder="ACCIONES" disabled />
+        </div>
       </div>
-      <div class="filters__item">
-        <input type="text" placeholder="NOMBRE" />
+      <div class="results">
+        <div class="results__item" v-for="drug in drugs" :key="drug.id">
+          <p>{{ drug.codigo }}</p>
+          <p>{{ drug.nombre }}</p>
+          <p>{{ drug.cantidad }}</p>
+          <p>{{ drug.fabricante }}</p>
+          <button>Ver detalles</button>
+        </div>
       </div>
-      <div class="filters__item">
-        <input type="text" placeholder="CANTIDAD" />
-      </div>
-      <div class="filters__item">
-        <input type="text" placeholder="FABRICANTE" />
-      </div>
-      <div class="filters__item">
-        <input type="text" placeholder="ACCIONES" disabled />
-      </div>
+    </template>
+    <div class="add__btn" @click="add" v-if="!modalActive">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        role="img"
+        width="1em"
+        height="1em"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 24 24"
+      >
+        <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+      </svg>
     </div>
-    <div class="results">
-      <div class="results__item" v-for="drug in drugs" :key="drug.id">
-        <p>{{ drug.codigo }}</p>
-        <p>{{ drug.nombre }}</p>
-        <p>{{ drug.cantidad }}</p>
-        <p>{{ drug.fabricante }}</p>
-        <button>Ver detalles</button>
+    <div class="add__modal" v-if="modalActive">
+      <div class="close__btn" @click="modalActive = false">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          role="img"
+          width="1em"
+          height="1em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 1024 1024"
+        >
+          <g transform="rotate(-90 512 512)">
+            <path
+              fill="currentColor"
+              d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4l-66.1.3c-4.4 0-8-3.5-8-8c0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4l66-.3c4.4 0 8 3.5 8 8c0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2c0 4.4-3.6 8-8 8z"
+            />
+          </g>
+        </svg>
       </div>
+      <div class="modal__title">
+        <h2>Nuevo medicamento</h2>
+      </div>
+      <form>
+        <div class="form__body">
+          <div class="form__field">
+            <label>Código</label>
+            <input type="text" />
+          </div>
+          <div class="form__field">
+            <label>Nombre</label>
+            <input type="text" />
+          </div>
+          <div class="form__field">
+            <label>Cantidad</label>
+            <input type="text" />
+          </div>
+          <div class="form__field">
+            <label>Fabricante</label>
+            <input type="text" />
+          </div>
+        </div>
+
+        <button type="submit">Agregar</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "VistaPreescripciones",
+  name: "VistaMedicamentos",
   data() {
     return {
       drugs: [
@@ -48,7 +108,20 @@ export default {
           fabricante: "Que se cho",
         },
       ],
+      modalActive: false,
+      drug: {
+        codigo: "",
+        nombre: "",
+        cantidad: "",
+        fabricante: "",
+      },
     };
+  },
+
+  methods: {
+    add() {
+      this.modalActive = true;
+    },
   },
 };
 </script>
@@ -57,7 +130,115 @@ export default {
 .medicamentos {
   height: 450px;
   overflow-y: hidden;
+  position: relative;
 }
+
+.add__btn {
+  background-color: #44b6fe;
+  border-radius: 50%;
+  position: fixed;
+  bottom: 1rem;
+  right: 5%;
+  width: 50px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  svg {
+    width: 2rem;
+    height: 2rem;
+    path {
+      fill: #fff;
+    }
+  }
+}
+
+.add__modal {
+  background-color: #fff;
+  width: 800px;
+  height: 500px;
+  border: 2px solid #44b6fe;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 1rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  .modal__title {
+    h2 {
+      text-align: center;
+    }
+  }
+
+  .close__btn {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    cursor: pointer;
+
+    svg {
+      width: 100%;
+      height: 100%;
+      path {
+        fill: red;
+      }
+    }
+  }
+
+  form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+
+    .form__body {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 4rem;
+      align-items: center;
+      margin-bottom: 2rem;
+    }
+
+    button {
+      cursor: pointer;
+      border: 2px solid transparent;
+      text-transform: uppercase;
+      display: block;
+      border-radius: 0.7rem;
+      background-color: #44b6fe;
+      color: #fff;
+      padding: 1rem 3rem;
+      font-weight: 600;
+      letter-spacing: 3px;
+      font-size: 18px;
+      transition: all 300ms ease;
+
+      &:hover {
+        background-color: #fff;
+        color: #44b6fe;
+        border-color: #44b6fe;
+      }
+    }
+  }
+}
+
+.form__field {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+}
+
 .filters {
   display: flex;
   justify-content: space-between;
