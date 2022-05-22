3c8d2e2f96f77dbb437ba8f2a7c514b170aab2d5
Juan Pablo Navarrete <jpablo.javier.nb@gmail.com>
2022-05-22 15:44:17 -0400

    Add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="add__modal" v-if="modalActive">
+    <div class="close__btn" @click="closeModal">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        role="img"
+        width="1em"
+        height="1em"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 1024 1024"
+      >
+        <g transform="rotate(-90 512 512)">
+          <path
+            fill="currentColor"
+            d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4l-66.1.3c-4.4 0-8-3.5-8-8c0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4l66-.3c4.4 0 8 3.5 8 8c0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2c0 4.4-3.6 8-8 8z"
+          />
+        </g>
+      </svg>
+    </div>
+    <div class="modal__title">
+      <h2>{{ title }}</h2>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    modalActive: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.add__modal {
+  background-color: #fff;
+  width: 800px;
+  height: 500px;
+  border: 2px solid #44b6fe;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 1rem;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  .modal__title {
+    h2 {
+      text-align: center;
+    }
+  }
+
+  .close__btn {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    cursor: pointer;
+
+    svg {
+      width: 100%;
+      height: 100%;
+      path {
+        fill: red;
+      }
+    }
+  }
+}
+</style>
--- src/views/VistaMedicamentos.vue
+++ src/views/VistaMedicamentos.vue
@@ -28,7 +28,7 @@
         </div>
       </div>
     </template>
-    <div class="add__btn" @click="add" v-if="!modalActive">
+    <div class="add__btn" @click="toggleModal" v-if="!modalActive">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         aria-hidden="true"
@@ -41,28 +41,11 @@
         <path fill="currentColor" d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
       </svg>
     </div>
-    <div class="add__modal" v-if="modalActive">
-      <div class="close__btn" @click="modalActive = false">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          role="img"
-          width="1em"
-          height="1em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 1024 1024"
-        >
-          <g transform="rotate(-90 512 512)">
-            <path
-              fill="currentColor"
-              d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4l-66.1.3c-4.4 0-8-3.5-8-8c0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4l66-.3c4.4 0 8 3.5 8 8c0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2c0 4.4-3.6 8-8 8z"
-            />
-          </g>
-        </svg>
-      </div>
-      <div class="modal__title">
-        <h2>Nuevo medicamento</h2>
-      </div>
+    <modal
+      title="Nuevo medicamento"
+      :modalActive="modalActive"
+      @close="toggleModal"
+    >
       <form>
         <div class="form__body">
           <div class="form__field">
@@ -85,16 +68,17 @@
 
         <blue-button type="submit">Agregar</blue-button>
       </form>
-    </div>
+    </modal>
   </div>
 </template>
 
 <script>
 import BlueButton from "@/components/BlueButton.vue";
+import Modal from "@/components/Modal.vue";
 
 export default {
   name: "VistaMedicamentos",
-  components: { BlueButton },
+  components: { BlueButton, Modal },
   data() {
     return {
       drugs: [
@@ -122,8 +106,8 @@ export default {
   },
 
   methods: {
-    add() {
-      this.modalActive = true;
+    toggleModal() {
+      this.modalActive = !this.modalActive;
     },
   },
 };
@@ -157,61 +141,20 @@ export default {
     }
   }
 }
-
-.add__modal {
-  background-color: #fff;
-  width: 800px;
-  height: 500px;
-  border: 2px solid #44b6fe;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  border-radius: 1rem;
-  padding: 1rem 2rem;
+form {
+  width: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  flex-wrap: wrap;
+  gap: 1rem;
 
-  .modal__title {
-    h2 {
-      text-align: center;
-    }
-  }
-
-  .close__btn {
-    width: 30px;
-    height: 30px;
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    cursor: pointer;
-
-    svg {
-      width: 100%;
-      height: 100%;
-      path {
-        fill: red;
-      }
-    }
-  }
-
-  form {
-    width: 100%;
+  .form__body {
     display: flex;
-    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 4rem;
     align-items: center;
-    gap: 1rem;
-
-    .form__body {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-      gap: 4rem;
-      align-items: center;
-      margin-bottom: 2rem;
-    }
+    margin-bottom: 2rem;
   }
 }
 
